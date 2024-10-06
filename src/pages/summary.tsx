07dfb06a3799b05c35dcc9bc4ced6b6d7006ed42
ysmt <shinya.yoshimoto.67@gmail.com>
2024-10-07 07:45:44 +0900

    feat: スタイル

--- src/pages/summary.tsx
+++ src/pages/summary.tsx
@@ -20,22 +20,40 @@ interface Data {
     }[];
   };
 }
+
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+const headingAccentStyles = {
+  color: "#663399",
+}
+
 const SummaryPage = ({ data }: { data: Data }) => {
   const answers = data.allRestApiScoresAnswersSummaries.nodes[0].answers;
 
   return (
-    <div>
-      <h1>Summaries</h1>
-      <ul>
-        {answers.map((answer, index) => (
-          <li key={index}>
-            <p>{answer.isStartPlayer ? "親" : "子"}{answer.symbolCount}符{answer.fanCount}飜{answer.isDraw ? "ツモ" : "ロン"}</p>
-            <p>正: {answer.count.true} / 誤: {answer.count.false}</p>
-            <br />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>Summaries</h1>
+      <div>
+        <ul>
+          {answers.map((answer, index) => (
+            <li key={index}>
+              <p>{answer.isStartPlayer ? "親" : "子"}{answer.symbolCount}符{answer.fanCount}飜{answer.isDraw ? "ツモ" : "ロン"}</p>
+              <p>正: {answer.count.true} / 誤: {answer.count.false}</p>
+              <br />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 };
 
